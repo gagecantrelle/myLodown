db9ac75b04826eaede2646e7148480c9a2b1fbd3
gagecantrelle <gagecantrelle1899@gmail.com>
2022-10-31 22:06:29 +0000

    asd

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ module.exports.identity = identity;
  * typeOf: check what type of value is given to the paramter
  * 
  * @param {any value} value: hold any type of value
+ * @return {string}: a string that hold the name of any value
  */
 
  function typeOf(value){
@@ -82,12 +83,12 @@ module.exports.identity = identity;
 module.exports.typeOf = typeOf;
 
 /**
- * first: will loop through a given array then return the first value in the array, a new array of value,
- * an empty array, or the given array
+ * first: will loop through a given array then return a set number of value determend by the given paramter
+ * from the beging of the array, a new array of value, an empty array, or the given array
  * 
  * @param {Array} arr: an array of values
  * @param {Number} num: a number given for deciding on what to return 
- * 
+ * @return {Array}: return an array filled with a set amount of values, an empthy array, or the given array;
  */
  function first(arr, num){
     //creat a empty array
@@ -127,12 +128,12 @@ module.exports.typeOf = typeOf;
 module.exports.first = first;
 
 /**
- * last: will loop through a given array then return the last value in the array, a new array of value,
- * an empty array, or the given array
+ * last: will loop through a given array then return a set number of value determend by the given paramter
+ * from the end of the array, a new array of value, an empty array, or the given array
  * 
  * @param {Array} arr: an array of values
  * @param {Number} num: a number given for deciding on what to return 
- * 
+ * @return {Array}: return an array filled with a set amount of values, an empthy array, or the given array;
  */
 function last(arr, num){
     //creat a empty array
@@ -177,7 +178,7 @@ module.exports.last = last;
  * 
  * @param {Array} arr: an array of values
  * @param {an Value} value: a given value to find in the array
- * 
+ * @return {Number}: return the index number of the given value, if the given value is not in the given array return -1
  */
  function indexOf(arr, value){
     //loop through all the array values
@@ -198,7 +199,7 @@ module.exports.last = last;
  * 
  * @param {Array} arr: an array of values
  * @param {an Value} value: a given value to find in the array
- * 
+ * @return {Boolean}: return true or false if the the the given array has the given value
  */
  function contains(arr, value){
     //loop through all values in array
@@ -218,7 +219,7 @@ module.exports.contains = contains;
  * unique: will loop through a given array then return a new array without duplicate values
  * 
  * @param {Array} array: an array of values
- * 
+ * @return {Array}: return the given array with no duplication
  */
  function unique(array){
     //retun new array with no duplicate
@@ -227,11 +228,12 @@ module.exports.contains = contains;
 module.exports.unique = unique;
 
 /**
- * filter: will loop through a given array then run a given function to filter out certan values then return a new array
+ * filter: will loop through a given array that will run a given function that will test each value in the array and
+ * if the function return true push that value into a new array. then return a new array of values
  * 
  * @param {Array} arr: an array of values
  * @param {Function} func: a function that runs a test
- * 
+ * @return {Array}: return array of value that contanis certant value determind by the given function
  */
  function filter(arr, func){
     //creat empty array
@@ -250,11 +252,12 @@ module.exports.unique = unique;
   module.exports.filter = filter;
 
 /**
- * reject: will loop through a given array then run a given function to remover certan values then return a new array
+ * reject: will loop through a given array that will run a given function that will test each value in the array and
+ * if the function return false push that value into a new array. then return a new array of values
  * 
  * @param {Array} arr: an array of values
  * @param {Function} func: a function that runs a test
- * 
+ * @return {Array}: return array of value that contanis certant value determind by the given function
  */
 function reject(arr, func){
     //creat empty array
@@ -273,13 +276,13 @@ function reject(arr, func){
 module.exports.reject = reject;
 
 /**
- * partition: will loop through a given array then run a given function to seperate certan values
- * then return a array with two array in it with one containg targted values the the other containg
- * the other values
+ * partition: will loop through a given array then run a given function to test each value, then
+ * creat an array that will hold two array. the frist one will hold values that the function is testing for.
+ * the second array will hold values that failed the function test
  * 
  * @param {Array} arr: an array of values
  * @param {Function} func: a function that runs a test
- * 
+ * @return {Array}: return a array containing two arrays, the frist array contains value that pase the function test, and the other array contains the values that failed the test
  */
  function partition(arr, func){
     //creat an array with two arrays in it
@@ -303,11 +306,11 @@ module.exports.partition = partition;
 
 /**
  * map: will loop through a given array then run a given function to creat a new array that
- * holds certant values that the given function is testing for
+ * holds values that the given function returns
  * 
  * @param {Collection} coll: an array or object of values
  * @param {Function} func: a function that runs a test
- * 
+ * @return {Collection}: return a object or array that is holds values return from the given function; 
  */
  function map(coll, func){
     //creaft a new array
@@ -338,7 +341,7 @@ module.exports.map = map;
  * 
  * @param {array} arr: an array of values
  * @param {Object value} prop: a object key value or key
- * 
+ * @return {array}: return a new array filled with targted values
  */
  function pluck(arr, prop){
     //creat a empty array
@@ -354,7 +357,7 @@ module.exports.pluck = pluck;
  * 
  * @param {collection} coll: an array or object of values
  * @param {function} func: a function that test a value then return true or false
- * 
+ * @return {Boolean}: retun true if all values in the given collection that pass through the given function return true
  */
 function every(coll, func){
     //determine if func was not passed in
@@ -405,11 +408,11 @@ module.exports.every = every;
 
 /**
  * some: will loop through a given array or object then run a given function that test each value
- * then return true or false depending if the some value given to the function return true 
+ * then return true or false depending if at least one value given to the function return true 
  * 
  * @param {collection} coll: an array or object of values
  * @param {function} func: a function that test a value then return true or false
- * 
+ * @return {Boolean}: retun true if all values in the given collection that pass through the given function return true
  */
  function some(coll, func){
     //determine if func was not passed in
@@ -464,7 +467,7 @@ module.exports.every = every;
  * @param {Array} arr: an array of values
  * @param {function} func: a function that test a value then return true or false
  * @param {seed value} seed: a value that will equal the accumlation value the given function
- * 
+ * @return {Any value}: return a single value after pushing in all values from the given array
  */
 function reduce(arr, func, seed){
     //creat tesult variable
@@ -496,7 +499,7 @@ module.exports.reduce = reduce;
  * extend: will add more values to the given object
  * 
  * @param {one or more Objects} obj: an object of values
- * 
+ * @return {Object}: return a obj that combines all given objects
  */
  function extend(...obj){
     // set obj equal to mutiple given objects
